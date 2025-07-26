1e63dd842b2fcb7bc0e629f195b24f98c7731a12
ai_anchorite <ai_anchorite@proton.me>
2025-07-27 01:42:32 +1200

    Add files via upload

---
+++ install.js
@@ -0,0 +1,49 @@
+module.exports = {
+  run: [
+    // nvidia GPU support only
+    {
+     when: "{{gpu !== 'nvidia'}}",
+     method: "notify",
+     params: {
+       html: "This app requires an NVIDIA GPU."
+     }, 
+      next: null
+    },
+
+    {
+      method: "shell.run",
+      params: {
+        message: [
+          "git clone https://github.com/ostris/ai-toolkit.git app",
+        ]
+      }
+    },
+    // Delete this step if your project does not use torch
+    {
+      method: "script.start",
+      params: {
+        uri: "torch.js",
+        params: {
+          venv: "env",                // Edit this to customize the venv folder path
+          path: "app",                // Edit this to customize the path to start the shell from
+          // xformers: true   // uncomment this line if your project requires xformers
+          // triton: true   // uncomment this line if your project requires triton
+          // sageattention: true   // uncomment this line if your project requires sageattention
+        }
+      }
+    },
+    // Edit this step with your custom install commands
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",                // Edit this to customize the venv folder path
+        path: "app",                // Edit this to customize the path to start the shell from
+        message: [
+          "uv pip install gradio devicetorch",
+          "uv pip install -r requirements.txt",
+          "uv pip install hf_xet"
+        ]
+      }
+    },
+  ]
+}
